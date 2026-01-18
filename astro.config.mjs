eaa0026ec7cea7de4f3f6826310e05c940173942
teddy.bear <teddy.bear@lgvoltup.com>
2026-01-18 19:34:24 +0900

    configure custom domain map.dongholab.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://gongdongho12.github.io',
+  site: 'https://map.dongholab.com',
   // base path 제거 - 루트에서 서빙
   output: 'static',
 });
